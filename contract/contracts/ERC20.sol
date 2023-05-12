f27feeff9165881bd12fbb34f196ad6d49032711
Simranjeet Singh <smrnjeet222@gmail.com>
2023-05-12 18:24:51 +0530

    tests

--- contract/contracts/ERC20.sol
+++ contract/contracts/ERC20.sol
@@ -1,11 +1,9 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract MyToken is ERC20 {
     uint8 decimals_;
 
     constructor(
@@ -16,8 +14,16 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
         decimals_ = _decimals;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function mint(uint _amount) external {
+        _mint(msg.sender, _amount);
+    }
+
+    function burn(uint _amount) external {
+        _burn(msg.sender, _amount);
+    }
+
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
     }
 
     function decimals() public view override returns (uint8) {
--- contract/test/marketPlace.ts
+++ contract/test/marketPlace.ts
@@ -1,19 +1,27 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { PokeCardERC1155, PokeMarketPlace, PokeCardERC721 } from "../typechain-types";
+import { Contract } from "ethers";
 
 const NativeAddress = "0x0000000000000000000000000000000000000000";
 
 describe("marketPlace", function () {
-    let PokeCardERC1155: any, PokeMarketPlace: any, erc20TestToken: any, clxnProxy: any, orderId: string;
+    let PokeCardERC1155: PokeCardERC1155, PokeCardERC721: PokeCardERC721, PokeMarketPlace: PokeMarketPlace, erc20TestToken: Contract, orderId: string;
+
     it("should deploy ERC20", async function () {
-        const TestToken = await ethers.getContractFactory("TestToken");
+        const TestToken = await ethers.getContractFactory("MyToken");
         erc20TestToken = await TestToken.deploy("Test Token for test", "TTT", 18);
         await erc20TestToken.deployed();
     });
-    it("Should deploy nft contract", async function () {
+    it("Should deploy ERC1155 nft contract", async function () {
         const factory = await ethers.getContractFactory("PokeCardERC1155");
         PokeCardERC1155 = await factory.deploy();
         await PokeCardERC1155.deployed();
+    });
+    it("Should deploy ERC721 nft contract", async function () {
+        const factory = await ethers.getContractFactory("PokeCardERC721");
+        PokeCardERC721 = await factory.deploy();
+        await PokeCardERC721.deployed();
     });
     it("Should deploy market place contract", async function () {
         const factory = await ethers.getContractFactory("PokeMarketPlace");
@@ -38,9 +46,15 @@ describe("marketPlace", function () {
         const [owner] = await ethers.getSigners();
         await PokeCardERC1155.mintById(owner.address, 5, 5);
         expect(await PokeCardERC1155.balanceOf(owner.address, 5)).to.equal(5);
-        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, 1);
+
+        await PokeCardERC721.setApprovalForAll(PokeMarketPlace.address, true);
+        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, true);
+
+        await PokeMarketPlace.addNftContractSupport(PokeCardERC721.address);
         await PokeMarketPlace.addNftContractSupport(PokeCardERC1155.address);
         await PokeMarketPlace.addTokenSupport(erc20TestToken.address);
+
+        expect(await PokeMarketPlace.nftContracts(PokeCardERC721.address)).to.equal(true);
         expect(await PokeMarketPlace.nftContracts(PokeCardERC1155.address)).to.equal(true);
         expect(await PokeMarketPlace.tokensSupport(erc20TestToken.address)).to.equal(true);
 
@@ -56,9 +70,9 @@ describe("marketPlace", function () {
         const topics = receipt.logs[1].topics;
         const event = interfaceTx.decodeEventLog("OrderCreated", data, topics);
         expect(event.seller).to.equal(owner.address);
-        for (const event of receipt.events) {
-            // console.log(`Event ${event.event} with args ${event.args}`);
-        }
+        // for (const event of receipt.events) {
+        //     // console.log(`Event ${event.event} with args ${event.args}`);
+        // }
 
         const OrderId = event.orderId;
 
@@ -74,10 +88,6 @@ describe("marketPlace", function () {
         const balance = await PokeCardERC1155.balanceOf(PokeMarketPlace.address, 6);
 
         console.log(balance);
-
-        await PokeMarketPlace.bulkBuy([OrderId], [5], {
-            value: ethers.utils.parseEther("5.0")
-        });
     });
 
 
@@ -85,14 +95,14 @@ describe("marketPlace", function () {
         const [owner] = await ethers.getSigners();
         await PokeCardERC1155.mintById(owner.address, 10, 10);
 
-        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, 1);
+        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, true);
         const tx = await PokeMarketPlace.placeOrderForSell(10, PokeCardERC1155.address, 8, "10000000000000000000", erc20TestToken.address, 0, 0);
         const receipt = await tx.wait();
 
         // const txCancel = await PokeMarketPlace.cancelOrder(1);
         // const receiptCancel = await txCancel.wait();
 
-        for (const event of receipt.events) {
+        for (const event of receipt.events!) {
             console.log(`Event ${event.event} with args ${event.args}`);
         }
         // expect(await PokeMarketPlace.cancelOrder(2)).to.equal(true);
@@ -113,7 +123,7 @@ describe("marketPlace", function () {
         const [owner, buyer, bidder1, bidder2] = await ethers.getSigners();
         await PokeCardERC1155.mintById(owner.address, tokenId, copies);
 
-        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, 1);
+        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, true);
         const endTime = parseInt((Date.now() / 1000).toFixed(2)) + 9000;
         const minPrice = ethers.utils.parseEther("0.01");
         const tx = await PokeMarketPlace.placeOrderForSell(tokenId, PokeCardERC1155.address, copies, minPrice, erc20TestToken.address, endTime, 0);
@@ -123,8 +133,8 @@ describe("marketPlace", function () {
         const topics = receipt.logs[1].topics;
         const event = interfaceTx.decodeEventLog("OrderCreated", data, topics);
         expect(event.seller).to.equal(owner.address);
-        for (const event of receipt.events) {
-            // console.log(`Event ${event.event} with args ${event.args}`);
+        for (const event of receipt.events!) {
+            console.log(`Event ${event.event} with args ${event.args}`);
         }
 
         orderId = event.orderId;
@@ -169,7 +179,7 @@ describe("marketPlace", function () {
         });
         const balanceAfter = await erc20TestToken.connect(bidder2).balanceOf(bidder2.address);
         console.log(balanceAfter.toString());
-        await PokeMarketPlace.connect(bidder2).withdrawRejectBid(orderId, 1, 0);
+        await PokeMarketPlace.connect(bidder2).withdrawRejectBid(orderId, 1, false);
         const balanceAfterWithdraw = await erc20TestToken.connect(bidder2).balanceOf(bidder2.address);
         console.log(balanceAfterWithdraw.toString());
     })
@@ -179,7 +189,7 @@ describe("marketPlace", function () {
         const [owner, buyer] = await ethers.getSigners();
         await PokeCardERC1155.mintById(owner.address, tokenId, copies);
 
-        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, 1);
+        await PokeCardERC1155.setApprovalForAll(PokeMarketPlace.address, true);
         const endTime = parseInt((Date.now() / 1000).toFixed(2)) + 600;
         const tx = await PokeMarketPlace.placeOrderForSell(tokenId, PokeCardERC1155.address, copies, ethers.utils.parseEther("1.0"), NativeAddress, 0, 0);
         const receipt = await tx.wait();
@@ -201,7 +211,7 @@ describe("marketPlace", function () {
         const tokenId = 21, copies = 5;
         const [owner, buyer, seller1, bidder1] = await ethers.getSigners();
         await PokeCardERC1155.mintById(seller1.address, tokenId, copies);
-        await PokeCardERC1155.connect(seller1).setApprovalForAll(PokeMarketPlace.address, 1);
+        await PokeCardERC1155.connect(seller1).setApprovalForAll(PokeMarketPlace.address, true);
         const endTime = parseInt((Date.now() / 1000).toFixed(2)) + 9000;
         const minPrice = ethers.utils.parseEther("0.01")
         const tx = await PokeMarketPlace.connect(seller1).placeOrderForSell(tokenId, PokeCardERC1155.address, copies, minPrice, NativeAddress, endTime, 1);
@@ -211,7 +221,7 @@ describe("marketPlace", function () {
         const topics = receipt.logs[1].topics;
         const event = interfaceTx.decodeEventLog("OrderCreated", data, topics);
         expect(event.seller).to.equal(seller1.address);
-        for (const event of receipt.events) {
+        for (const event of receipt.events!) {
             // console.log(`Event ${event.event} with args ${event.args}`);
         }
 
@@ -236,25 +246,25 @@ describe("marketPlace", function () {
 
         // await PokeMarketPlace.connect(bidder1).nativeWithdrawBid(orderId, 0);
 
-        await PokeMarketPlace.connect(seller1).withdrawRejectBid(orderId, 0, 1);
+        await PokeMarketPlace.connect(seller1).withdrawRejectBid(orderId, 0, true);
 
     });
 
-    it("withdraw TOKEN", async function () {
-        const provider = ethers.getDefaultProvider();
-        const [owner, buyer, seller1, bidder1] = await ethers.getSigners();
-        const beforeBalance = await provider.getBalance(PokeMarketPlace.address);
-        console.log("Before balance", beforeBalance.toString());
-        await PokeMarketPlace.withdrawMoney((ethers.utils.parseEther("5")), NativeAddress);
-        const afterBalance = await provider.getBalance(PokeMarketPlace.address);
-        console.log("after balance", afterBalance);
+    // it("withdraw TOKEN", async function () {
+    //     const provider = ethers.getDefaultProvider();
+    //     const [owner, buyer, seller1, bidder1] = await ethers.getSigners();
+    //     const beforeBalance = await provider.getBalance(PokeMarketPlace.address);
+    //     console.log("Before balance", beforeBalance.toString());
+    //     await PokeMarketPlace.withdrawMoney((ethers.utils.parseEther("5")), NativeAddress);
+    //     const afterBalance = await provider.getBalance(PokeMarketPlace.address);
+    //     console.log("after balance", afterBalance);
 
-        console.log("ERC20 ------------- ERC20");
-        const erc20BeforeBalance = await erc20TestToken.balanceOf(PokeMarketPlace.address);
-        console.log("Before balance", erc20BeforeBalance.toString());
-        await PokeMarketPlace.withdrawMoney(erc20BeforeBalance, erc20TestToken.address);
-        const erc20AfterBalance = await erc20TestToken.balanceOf(PokeMarketPlace.address);
-        console.log("after balance", erc20AfterBalance);
-    });
+    //     console.log("ERC20 ------------- ERC20");
+    //     const erc20BeforeBalance = await erc20TestToken.balanceOf(PokeMarketPlace.address);
+    //     console.log("Before balance", erc20BeforeBalance.toString());
+    //     await PokeMarketPlace.withdrawMoney(erc20BeforeBalance, erc20TestToken.address);
+    //     const erc20AfterBalance = await erc20TestToken.balanceOf(PokeMarketPlace.address);
+    //     console.log("after balance", erc20AfterBalance);
+    // });
 
 });
